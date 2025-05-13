49b04ff50e02ad4881a7b619033248d44dda10e4
Jeremiah Russell <jerry@jrussell.ie>
2025-05-13 13:44:02 +0100

    ✨ feat(captval): add hcaptcha feature flag
    - introduce hcaptcha feature flag in Cargo.toml
    - expose additional hcaptcha-related modules and types
    
    ♻️ refactor(hcaptcha): update module visibility and documentation
    
    - add #[doc(hidden)] to certain modules for cleaner documentation
    - adjust module visibility to align with feature flag usage

--- crates/captval/src/hcaptcha.rs
+++ crates/captval/src/hcaptcha.rs
@@ -1,21 +1,23 @@
+#[doc(hidden)]
 mod client_response;
 mod code;
 #[cfg(not(feature = "ext"))]
+#[doc(hidden)]
 mod secret;
 #[cfg(feature = "ext")]
+#[doc(hidden)]
 mod secret_ext;
+#[doc(hidden)]
 mod sitekey;
 
 // pub(crate) use client_response::ClientResponse;
 pub use code::Code;
 
-#[doc(hidden)]
+pub use client_response::ClientResponse;
 #[cfg(not(feature = "ext"))]
 pub(crate) use secret::Secret;
 #[allow(unused_imports)]
-#[doc(hidden)]
 #[cfg(feature = "ext")]
 pub(crate) use secret_ext::Secret;
 #[allow(unused_imports)]
-#[doc(hidden)]
 pub(crate) use sitekey::Sitekey;
