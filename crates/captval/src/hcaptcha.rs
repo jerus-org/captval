9a742ea61f90cc270db27f7e246e249e08d38c95
Jeremiah Russell <jerry@jrussell.ie>
2025-05-13 12:34:20 +0100

    💄 style(hcaptcha): allow unused imports for certain features
    - add #[allow(unused_imports)] attribute to suppress warnings for conditional imports
    - ensure clean compilation with feature flags enabled or disabled

--- crates/captval/src/hcaptcha.rs
+++ crates/captval/src/hcaptcha.rs
@@ -12,8 +12,10 @@ pub use code::Code;
 #[doc(hidden)]
 #[cfg(not(feature = "ext"))]
 pub(crate) use secret::Secret;
+#[allow(unused_imports)]
 #[doc(hidden)]
 #[cfg(feature = "ext")]
 pub(crate) use secret_ext::Secret;
+#[allow(unused_imports)]
 #[doc(hidden)]
 pub(crate) use sitekey::Sitekey;
