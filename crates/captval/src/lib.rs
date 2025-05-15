2632d8c56ced4e2ca0c46e0092b1418ccf076274
Jeremiah Russell <jerry@jrussell.ie>
2025-05-15 14:07:01 +0100

    ♻️ refactor(release): update release configuration for captval
    - remove pre-release replacements for CHANGELOG.md and README.md in captval release.toml
    - update version in src/lib.rs to 0.1.0
    
    🐛 fix(release): adjust sonar-project properties for captval_derive
    
    - add pre-release replacements for sonar-project.properties in captval_derive release.toml
    - create sonar-project.properties file for captval_derive with initial setup
    
    🔧 chore(config): relocate sonar-project.properties
    
    - move sonar-project.properties to crates/captval directory for better organization

--- crates/captval/src/lib.rs
+++ crates/captval/src/lib.rs
@@ -309,7 +309,7 @@
 //!
 //! ```toml
 //! [dependency]
-//! captval = { version = "3.0.26", default-features = false }
+//! captval = { version = "0.1.0", default-features = false }
 //! ```
 //!
 //! The following feature flags are available:
